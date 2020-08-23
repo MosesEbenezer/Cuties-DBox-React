e85b001850c65af1315ab824e37372d677717c0b
Moses Ebenezer <moseschimdike@gmail.com>
2020-08-23 07:59:23 +0100

    Updates

---
+++ precache-manifest.b060307a379e155a8ee87fd912c4f1e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0448927140a554b1479aa6f428e7bdcb",
+    "url": "/cuties-dbox-react/index.html"
+  },
+  {
+    "revision": "a276991bc646bc15a534",
+    "url": "/cuties-dbox-react/static/css/main.8c7f4284.chunk.css"
+  },
+  {
+    "revision": "fbb789ef4b7e71aa7a9e",
+    "url": "/cuties-dbox-react/static/js/2.3d137e5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cuties-dbox-react/static/js/2.3d137e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a276991bc646bc15a534",
+    "url": "/cuties-dbox-react/static/js/main.270ef766.chunk.js"
+  },
+  {
+    "revision": "f8165275dfecad0d0638",
+    "url": "/cuties-dbox-react/static/js/runtime-main.babbebf7.js"
+  }
+]);
